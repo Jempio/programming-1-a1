abe2f1354dd2808d04d8b65ebf82eac726839610
Jempio <66966887+Jempio@users.noreply.github.com>
2020-12-09 18:28:55 -0800

    Finalized pokemon quiz

--- pokemonQuiz.py
+++ pokemonQuiz.py
@@ -17,32 +17,32 @@ if q1 == "C":
 else:
     print("Dang nice try.")
 
-q2 = (input("What level does Weedle evolve at? (NUMBER)\n")).upper().strip("!,.?")
+q2 = (input("What level does Weedle evolve at?\n")).upper().strip("!,.?")
 if q2 == "7" or q2 == "SEVEN":
     print("Good job!")
     score += 1
 else:
-    print("Good try.")
+    print("Sorry! Weedle evolves at level 7.")
 
 q3 = input("Who is the 5th Gym Leader in Gen 3?\n").upper().strip("!,.?")
 if q3 == "NORMAN":
     print("Wow!")
     score += 1
 else:
-    print("Unlucky.")
+    print("Nope, Norman is the 5th Gym Leader")
 
 q4 = input("Who is the main villan in Pokemon Black and White?\n").upper().strip("!,.?")
 if q4 == "GHETSIS":
     print("Holy nice job!")
     score += 1
 else:
-    print("Aw good try")
+    print(f"{q4} was a villain in a different timeline.")
 
 q5 = (input("What is the base pp of Roost?\n")).upper().strip("!,.?")
 if q5 == "10" or q5 == "TEN":
     print("Ayy")
     score += 1
 else:
-    print("Man.")
+    print(f"Roost has a base pp of 10 not {q5}.")
 
 print(f"Okay that's the end of the quiz.\n Your final score is {score}/5\n That's {int(score / 5 * 100)}%!")
